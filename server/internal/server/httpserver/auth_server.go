e9aded23acf795bf4616a9c956dedf7c4d3ea266
Алексей Кузьмин <akuzmin340@gmail.com>
2025-06-17 08:56:08 +0200

    refresh endpoint

--- server/internal/server/httpserver/auth_server.go
+++ server/internal/server/httpserver/auth_server.go
@@ -8,6 +8,7 @@ import (
 
 type authService interface {
 	Login(ctx context.Context, username, password string) (*entity.Tokens, error)
+	UpdateTokens(ctx context.Context, refresh string) (*entity.Tokens, error)
 }
 
 type AuthServer struct {
@@ -27,6 +28,19 @@ func (s *AuthServer) login(w http.ResponseWriter, r *http.Request) {
 	password := r.FormValue("password")
 
 	tokens, err := s.authService.Login(ctx, email, password)
+	if err != nil {
+		writeAndLogErr(ctx, w, err)
+		return
+	}
+
+	writeJson(ctx, w, tokens, http.StatusOK)
+}
+
+func (s *AuthServer) refreshToken(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	refreshToken := r.FormValue("refresh_token")
+
+	tokens, err := s.authService.UpdateTokens(ctx, refreshToken)
 	if err != nil {
 		writeAndLogErr(ctx, w, err)
 		return
--- server/internal/server/httpserver/routes.go
+++ server/internal/server/httpserver/routes.go
@@ -14,17 +14,18 @@ const (
 )
 
 func (s *Server) RegisterRoutes(rtr *mux.Router) {
-	rtr.HandleFunc("/login", s.authServer.login)
+	rtr.HandleFunc("/auth/login", s.authServer.login)
+	rtr.HandleFunc("/auth/refresh", s.authServer.refreshToken)
 
 	rtr.HandleFunc("/users", s.authMw.withAuth(s.userServer.New, entity.UserRoleAdmin)).Methods(post)
 	rtr.HandleFunc("/users", s.authMw.withAuth(s.userServer.Update, entity.UserRoleAdmin)).Methods(put)
 	rtr.HandleFunc("/users", s.authMw.withAuth(s.userServer.GetAll, entity.UserRoleAdmin)).Methods(get)
 	rtr.HandleFunc("/users", s.authMw.withAuth(s.userServer.Delete, entity.UserRoleAdmin)).Methods(del)
 
-	rtr.HandleFunc("/order", s.authMw.withAuth(s.orderServer.New, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(post)
-	rtr.HandleFunc("/order", s.authMw.withAuth(s.orderServer.GetAll, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(get)
-	rtr.HandleFunc("/order/by-partner", s.authMw.withAuth(s.orderServer.GetByPartner, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(get)
-	rtr.HandleFunc("/order/discount", s.authMw.withAuth(s.orderServer.CalcDiscount, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(get)
+	rtr.HandleFunc("/orders", s.authMw.withAuth(s.orderServer.New, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(post)
+	rtr.HandleFunc("/orders", s.authMw.withAuth(s.orderServer.GetAll, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(get)
+	rtr.HandleFunc("/orders/by-partner", s.authMw.withAuth(s.orderServer.GetByPartner, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(get)
+	rtr.HandleFunc("/orders/discount", s.authMw.withAuth(s.orderServer.CalcDiscount, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(get)
 
 	rtr.HandleFunc("/partners", s.authMw.withAuth(s.partnerServer.New, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(post)
 	rtr.HandleFunc("/partners", s.authMw.withAuth(s.partnerServer.GetAll, entity.UserRoleAdmin, entity.UserRoleManager)).Methods(get)
